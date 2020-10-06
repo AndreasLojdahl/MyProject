df56d405efa84a50e5ff96587bc4dc1bc01588ef
Andreas LÖjdahl <andreas.lojdahl@gmail.com>
2020-10-06 10:20:39 +0200

    made Storage file into a Storage class with a singleton pattern

--- classes/data/Storage.js
+++ classes/data/Storage.js
@@ -1,18 +1,29 @@
 const Character = require("../characters/Character");
 
-let playerCharacters = [];
 
-const addPlayerCharacter = (character) => {
-    playerCharacters.push(character)
+module.exports = class Storage {
+    
+    StorageHasBeenInstantieted = false;
+
+    playerCharacters = [];
+    
+    constructor(){
+        if(Storage.StorageHasBeenInstantieted){
+            throw new Error('You can only create one instance of Storage!')
+        }
+        Storage.StorageHasBeenInstantieted = true;
+    }
+
+    getPlayerCharacters = () => {
+        return playerCharacters;
+    }
+
+    deletePlayerCharacter = (name) => {
+        let newList = playerCharacters.filter(char => char.name !== name )
+        playerCharacters = newList;
+    }
+
+    addPlayerCharacter = (character) => {
+        playerCharacters.push(character)
+    }
 }
-
-const deletePlayerCharacter = (name) => {
-    let newList = playerCharacters.filter(char => char.name !== name )
-    playerCharacters = newList;
-}
-
-const getPlayerCharacters = () => {
-    return playerCharacters;
-}
-
-module.exports = {playerCharacters, addPlayerCharacter, deletePlayerCharacter , getPlayerCharacters}
